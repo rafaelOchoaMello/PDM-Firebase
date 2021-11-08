55fedf3681cced133648af6e9eb723c8ea66b823
rafaelochoamello <rafaelochoamello@gmail.com>
2021-11-07 22:11:57 -0300

    🔨 Card Component

--- src/components/CardTemplate/index.jsx
+++ src/components/CardTemplate/index.jsx
@@ -1,14 +1,40 @@
 import React from "react"
+import './CardTemplate.css'
 
 const CardTemplate = () => {
+    // function moneyFormat(value) {
+    //     return Intl.NumberFormat('pt-br', {
+    //         style: 'currency',
+    //         currency: 'BRL',
+    //     }).format(value)
+    // }
+
     return (
+
+
         <>
-            <div className="cardContainer">
-                <div className="cardHeader">
-                    <p>Nome da carta</p>
+            <div style={{ display: "flex", flexFlow: "column" }}>
+                <div className="cardContainer">
+                    <div className="cardHeader">
+                        <p>Nome da carta</p>
+                    </div>
+                    <div className="cardImage">
+                        <img src="https://www.w3schools.com/bootstrap4/img_avatar6.png" alt="" srcset="" />
+
+                    </div>
+                    <div className="cardBody">
+                        <p>Jogo</p>
+                        <p className="rarityTitle">Raridade</p>
+                        <p>(1/10)</p>
+                        <p className='priceStyle'>Valor</p>
+                    </div>
                 </div>
-                <div className="cardImage"></div>
-                <div className="cardBody"></div>
+                <div style={{ display: "flex", flexFlow: "row" }}>
+                    <div className="optContainer rmvIcon" onClick={() => { alert('rmvIcon') }}>
+                    </div>
+                    <div className="optContainer">
+                    </div>
+                </div>
             </div>
         </>
     )
--- src/components/ContentContainer/index.jsx
+++ src/components/ContentContainer/index.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import './ContentContainer.css'
+import CardTemplate from '../CardTemplate'
 
 const ContentContainer= ({ children }) => {
     return (
         <div className="ContentContainer">
-            <div className="insideContent" />
+            <div className="insideContent">
+                <CardTemplate />
+                <CardTemplate />
+                <CardTemplate />
+                <CardTemplate />
+            </div>
         </div>
     )
 }
